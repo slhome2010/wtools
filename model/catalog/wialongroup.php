c0e784f9f85445c95d7996bce760b61c2c6b054b
slhome2010 <slhome2010@mail.ru>
2018-12-26 16:54:10 +0600

    Commit by GitKraken
    Commit by GitKraken for test

--- model/catalog/wialongroup.php
+++ model/catalog/wialongroup.php
@@ -7,41 +7,41 @@ class ModelCatalogWialongroup extends Model {
         $this->db->query("INSERT INTO `" . DB_PREFIX . "wialongroups` SET wialon_groupname = '" . $this->db->escape($data['wialon_groupname']) . "', wialon_group_off = '" . (int) $data['wialon_group_off'] . "', "
                 . "status = '" . (int) $data['status'] . "', server_id = '" . (int) $data['server_id'] . "',  wialon_id = '" . (int) $data['wialon_id']
                 . "', owner_id = '" . (int) $data['owner_id'] . "', tarif_id = '" . (int) $data['tarif_id'] . "', discount_id = '" . (int) $data['discount_id'] . "'");
+
         $wialon_group_id = $this->db->getLastId();
 
-        // $this->db->query("INSERT INTO `" . DB_PREFIX . "wialon_group_description` SET wialon_group_id = '" . (int)$wialon_group_id . "', description = '" . $this->db->escape($data['description']) . "', contactname = '" . $this->db->escape($data['contactname']) . "', telephone = '" . $this->db->escape($data['telephone']) . "', email = '" . $this->db->escape($data['email']) . "', address = '" . $this->db->escape($data['address']) . "', bin = '" . $this->db->escape($data['bin']) .  "'");
-
         $this->cache->delete('wialon_group');
+
         return $wialon_group_id;
     }
 
     public function editWialongroup($wialon_group_id, $data) {
+
         $this->db->query("UPDATE `" . DB_PREFIX . "wialongroups` SET wialon_groupname = '" . $this->db->escape($data['wialon_groupname']) . "', wialon_group_off = '" . (int) $data['wialon_group_off'] . "', "
                 . "status = '" . (int) $data['status'] . "', server_id = '" . (int) $data['server_id'] . "', server_id = '" . (int) $data['server_id'] . "', wialon_id = '" . (int) $data['wialon_id']
-                . "', owner_id = '" . (int) $data['owner_id'] . "', tarif_id = '" . (int) $data['tarif_id'] . "', discount_id = '" . (int) $data['discount_id'] . "' WHERE wialon_group_id = '" . (int) $wialon_group_id . "'");
-
-        //  $this->db->query("UPDATE `" . DB_PREFIX . "wialon_group_description` SET description = '" . $this->db->escape($data['description']) . "', contactname = '" . $this->db->escape($data['contactname'])
-        //         . "', telephone = '" . $this->db->escape($data['telephone']) . "', email = '" . $this->db->escape($data['email']) . "', address = '" . $this->db->escape($data['address']) . "', bin = '" . $this->db->escape($data['bin']) . "' WHERE wialon_group_id = '" . (int) $wialon_group_id . "'");
+                . "', owner_id = '" . (int) $data['owner_id'] . "', tarif_id = '" . (int) $data['tarif_id'] . "', discount_id = '" . (int) $data['discount_id'] . "'"
+                . " WHERE wialon_group_id = '" . (int) $wialon_group_id . "'");
 
         $this->cache->delete('wialon_group');
     }
 
     public function deleteWialongroup($wialon_group_id) {
+
         $this->db->query("DELETE FROM " . DB_PREFIX . "wialongroups WHERE wialon_group_id = '" . (int) $wialon_group_id . "'");
-        //  $this->db->query("DELETE FROM " . DB_PREFIX . "wialon_group_description WHERE wialon_group_id = '" . (int) $wialon_group_id . "'");
 
         $this->cache->delete('wialon_group');
     }
 
     public function getWialongroup($wialon_group_id) {
-        $query = $this->db->query("SELECT *, (SELECT s.servername FROM `" . DB_PREFIX . "server` s WHERE s.server_id = wg.server_id) AS servername FROM " . DB_PREFIX . "wialongroups wg WHERE wg.wialon_group_id = '" . (int) $wialon_group_id . "'");
+
+        $query = $this->db->query("SELECT *, (SELECT s.servername FROM `" . DB_PREFIX . "server` s WHERE s.server_id = wg.server_id) AS servername FROM "
+                . DB_PREFIX . "wialongroups wg WHERE wg.wialon_group_id = '" . (int) $wialon_group_id . "'");
 
         return $query->row;
     }
 
     public function getWialongroups($data = array()) {
-        //$sql = "SELECT * FROM " . DB_PREFIX . "wialon_group";
-
+     
         $sql = "SELECT * , (SELECT s.servername FROM `" . DB_PREFIX . "server` s WHERE s.server_id = wg.server_id) AS servername FROM " . DB_PREFIX . "wialongroups wg";
 
         if (!empty($data['filter_name'])) {
